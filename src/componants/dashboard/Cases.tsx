2df7f90660657dfc88e61120acd7ef62fe490930
Marc-Ruizenaar <info@marcruizenaar.com>
2025-03-03 11:44:31 +0100

    Dashboard page

---
+++ src/componants/dashboard/Cases.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { IoIosArrowForward } from "react-icons/io";
+
+export default function Cases() {
+  return (
+      <section className="cta">
+        <a href="./" className="cards cases">
+          <div className="header">
+            <h2>Cases</h2>
+            <span>
+              23
+              <IoIosArrowForward />
+            </span>
+          </div>
+
+          <div className="inner">
+            <img src="/knie.png" alt="Knee" />
+            <div>
+              <h3>Request for joint injection</h3>
+              <p>Ongoing chat</p>
+            </div>
+            <IoIosArrowForward />
+          </div>
+        </a>
+
+        <a href="./" className="cards messages">
+          <h2>Messages</h2>
+          <span>
+            0
+            <IoIosArrowForward />
+          </span>
+        </a>
+
+        <a href="./" className="cards messages">
+          <h2>Booked appointments</h2>
+          <span>
+            2
+            <IoIosArrowForward />
+          </span>
+        </a>
+      </section>
+  );
+}
---
+++ src/componants/dashboard/Categories.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+export default function Categories() {
+  const categories = [
+    {
+      link: "./",
+      title: "Seek Care",
+    },
+    {
+      link: "./",
+      title: "Vaccinations",
+    },
+    {
+      link: "./",
+      title: "Renew prescription",
+    },
+    {
+      link: "./",
+      title: "Facilities",
+    },
+    {
+      link: "./",
+      title: "Health centers",
+    },
+  ];
+  return (
+    <ul className="categories">
+      {categories.map((categorie) => {
+        return (
+          <li key={categorie.title}>
+            <a href={categorie.link} aria-label={`Go to ${categorie.title}`}>{categorie.title}</a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
